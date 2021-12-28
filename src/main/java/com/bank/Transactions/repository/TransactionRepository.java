bee2a95f7998d310d3eac4b4ee5ffcb305be42eb
Anoop <Anoop@NTP-LAP-226.chn.nestgroup.net>
2021-12-28 16:48:50 +0530

    first commit

---
+++ src/main/java/com/bank/Transactions/repository/TransactionRepository.java
@@ -0,0 +1,5 @@
+package com.bank.Transactions.repository;
+
+public interface TransactionRepository {
+
+}
---
+++ src/test/java/com/bank/Transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.Transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
